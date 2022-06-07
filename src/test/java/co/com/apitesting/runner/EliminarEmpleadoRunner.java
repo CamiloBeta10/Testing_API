d3d63ecbbcd4783f7323569c68f388c27adae325
CamiloBeta10 <56454732+CamiloBeta10@users.noreply.github.com>
2022-06-06 20:11:01 -0500

    Update EliminarEmpleadoRunner.java

--- src/test/java/co/com/apitesting/runner/EliminarEmpleadoRunner.java
+++ src/test/java/co/com/apitesting/runner/EliminarEmpleadoRunner.java
@@ -1,4 +1,13 @@
 package co.com.apitesting.runner;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resources/features/eliminar_Empleado.feature",
+        glue = "co/com/apitesting/stepdefinitions",
+        snippets = SnippetType.CAMELCASE)
 public class EliminarEmpleadoRunner {
 }
